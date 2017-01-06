c8e225c5159e0fce01bd515833fecd8be5ad83a9
likearockets <aczech81@gmail.com>
2017-01-06 20:34:37 +0100

    Update main.cpp

--- zadania/rekurencja/1/main.cpp
+++ zadania/rekurencja/1/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
-	for (short unsigned l1 = 1; l1 <= 50; l1++) cout << l1 <<" ";
+	for (short unsigned l1 = 1; l1 <= 50; l1++) cout << l1 <<" "; 			
 	_getch();
 	return 0;
 }
+
+
+/* czytelnosc rowna zeru 
+jakies l1 ktore podobne do M  
+czemu wszytko w mainie
+
+eh ocena: dwa na szynach
+i uwaga do dzienniczka */
